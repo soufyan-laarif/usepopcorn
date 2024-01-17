b8f3b263cc7ae2129028de7ce82db470dbc61b9c
Soufian2021 <soufyanlaarif7@gmail.com>
2024-01-17 17:50:56 +0100

    Using props as a component API and apply some PropTypes for data type validation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color={"blue"} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} ⭐</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={3} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating
+      maxRating={6}
+      color={"red"}
+      size={24}
+      className={"Test"}
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
